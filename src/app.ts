ae6fc47525d3b2fc167c08ef3137b4c8a4cd4f2a
Damian Gamracy <damiangamracy@gmail.com>
2019-06-21 22:31:01 +0200

    test

--- src/app.ts
+++ src/app.ts
@@ -11,13 +11,13 @@ class App {
     this.config();
   }
 
-  private config(): void {
+  private config() {
     this.app.set('port', process.env.SERVER_PORT || 3000);
 
     // support for application/json
     this.app.use(bodyParser.json());
 
-    // routing
+    //    srouting
     this.app.use('/', new ArticleRoute().router);
     this.app.use('/', new HealthRoute().router);
   }
